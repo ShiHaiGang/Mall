b38039630967e0a83e49222fb0d058a236393aa7
Shi Hai Gang <haigang.shi@foxmail.com>
2019-12-07 22:03:32 +0800

    订单效果完成

--- src/views/Order-copy.vue
+++ src/views/Order-copy.vue
@@ -1,46 +1,32 @@
 <!-- HTML -->
 <template>
   <div class="order">
-    <!-- 背景图片 -->
-    <nav
-      ref="nav"
-      :style="{ backgroundImage: image, transform: maxYScale }"
-    ></nav>
+    <nav ref="nav" :style="{ backgroundImage: image, transform: scale }"></nav>
+    <menu :style="{ height: menuHeight }">
+      <div class="img_container">
+        <img src="../assets/3.png" alt="" />
+      </div>
+      <h5>Love葙逢</h5>
+      <p>关注 1 <em>|</em> 粉丝 1</p>
+      <ul class="use_label">
+        <li>15后</li>
+        <li>Lv.9</li>
+        <li>上海</li>
+        <li>狮子座</li>
+      </ul>
+    </menu>
+    <!-- 背景模糊遮罩层 -->
+    <div class="bg_layer" :style="{ transform: maxY }"></div>
     <!-- tab 滚动展示层 -->
     <Scroll
       class="tab_wrapper"
       :probeType="3"
       :listenScroll="true"
       @scroll="onScroll"
+      :style="{ top: tabHeight }"
     >
-      <!-- 用户信息 -->
-      <menu :style="{ height: menuHeight }">
-        <div class="img_container">
-          <img src="../assets/3.png" alt="" />
-        </div>
-        <h5>Love葙逢</h5>
-        <p>关注 1 <em>|</em> 粉丝 1</p>
-        <ul class="use_label">
-          <li>15后</li>
-          <li>Lv.9</li>
-          <li>上海</li>
-          <li>狮子座</li>
-        </ul>
-      </menu>
-      <!-- 订单中心 -->
-      <ol class="use_tab">
-        <hgroup class="border_bottom_1px">
-          <h6
-            v-for="(item, index) in tabArr"
-            :key="index"
-            :class="{ active: item.state }"
-            @click="active(item)"
-          >
-            {{ item.title }}
-          </h6>
-        </hgroup>
-        <!-- 数据列表 -->
-        <li v-for="(item, index) in 15" :key="index">
+      <ol>
+        <li v-for="(item, index) in 25" :key="index">
           <img src="../assets/item_title.png" alt="" />
           全部订单
         </li>
@@ -53,47 +39,48 @@
 <script type="text/javascript">
 import Scroll from "components/scroll";
 
-// 预留头部高度
-const headerHeight = 50;
-// 提高20px的圆角
-const borderTopRadius = 20;
-
 export default {
   data() {
     return {
       navHeight: 0,
       scrollY: 0,
-      maxYScale: 0,
-      tabArr: [
-        { title: "待付款", state: true },
-        { title: "待收货", state: false },
-        { title: "待评价", state: false }
-      ]
+      maxY: 0,
+      scale: 0
     };
   },
   props: [],
   watch: {
     scrollY(newY, oldY) {
-      // 上推效果
+      // 预留头部高度
+      let headerHeight = 50;
+      // 提高20px的圆角
+      let borderTopRadius = 20;
+      // Math.max() 函数返回一组数中的最大值
       /* eslint-disable-next-line */
       let maxY = Math.max(-this.navHeight + headerHeight + borderTopRadius, newY);
-      this.maxYScale = `translate3d(0, ${maxY}px, 0)`;
-      // 下拉效果
+      this.maxY = `translate3d(0, ${maxY}px, 0)`;
+      // 使用 v-bind:style, Vue.js 会自动侦测并添加相应的前缀。
+      // this.$refs.layer.style["transform"] = `translate3d(0, ${maxY}px, 0)`;
       const percent = Math.abs(newY / this.navHeight);
       if (newY > 0) {
         // 下拉背景放大
         let scale = 1 + percent;
-        this.maxYScale = `scale(${scale})`;
+        this.scale = `scale(${scale})`;
+      }
+      if (newY < maxY) {
+        // 判断滚动到了顶部
+        this.$refs.nav.style["zIndex"] = 10;
+        this.$refs.nav.style["paddingTop"] = 0;
+        this.$refs.nav.style["height"] = `${headerHeight}px`;
+      } else {
+        // 还原
+        this.$refs.nav.style["height"] = 0;
+        this.$refs.nav.style["zIndex"] = 0;
+        this.$refs.nav.style["paddingTop"] = "90%";
       }
     }
   },
   methods: {
-    active(item) {
-      this.tabArr.forEach(e => {
-        e.state = false;
-      });
-      item.state = true;
-    },
     // 监听 scroll 事件
     onScroll(pos) {
       // console.log(pos);
@@ -106,7 +93,10 @@ export default {
       return `url(${img})`;
     },
     menuHeight() {
-      return `${this.navHeight - headerHeight - borderTopRadius}px`;
+      return `${this.navHeight - 50}px`;
+    },
+    tabHeight() {
+      return `${this.navHeight}px`;
     }
   },
   mounted() {
@@ -130,38 +120,22 @@ export default {
 }
 nav {
   position: relative;
-  /*宽高比为10:8*/
+  overflow: hidden;
+  /*宽高比为10:9*/
   width: 100%;
   height: 0;
-  z-index: -1;
-  padding-top: 80%;
+  padding-top: 90%;
   // background: url(../assets/my_bgc.jpg) no-repeat;
   background-repeat: no-repeat;
   background-size: cover;
   transform-origin: top;
-  // 背景遮光层
-  &::before {
-    position: absolute;
-    display: block;
-    content: "";
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.5);
-  }
-}
-.tab_wrapper {
-  position: absolute;
-  top: 50px;
-  bottom: 0;
-  height: auto;
-  overflow: hidden;
 }
 menu {
+  position: fixed;
+  top: 50px;
   width: 100%;
+  height: auto;
   padding: 24px;
-  color: #fff;
   // background-color: rgba(0, 0, 0, 0.5);
   .img_container {
     width: 80px;
@@ -175,27 +149,30 @@ menu {
     }
   }
   h5 {
-    // opacity: 0.8;
+    opacity: 0.8;
+    color: #fff;
     font-size: 20px;
     font-weight: 500;
     line-height: 50px;
   }
   p {
-    // opacity: 0.8;
+    opacity: 0.8;
+    color: #fff;
     font-size: 12px;
     & > em {
       display: inline-block;
       text-align: center;
-      opacity: 0.8;
+      opacity: 0.5;
       width: 20px;
     }
   }
   .use_label {
+    color: #fff;
     display: flex;
     flex-wrap: wrap;
     margin-top: 10px;
     li {
-      // opacity: 0.8;
+      opacity: 0.8;
       font-size: 12px;
       padding: 3px 12px;
       margin-right: 5px;
@@ -213,33 +190,23 @@ menu {
     }
   }
 }
-.use_tab {
-  color: #cbcbcb;
+.bg_layer {
+  position: relative;
+  height: 100%;
+  background: #f4f4f4;
+  // background: #34d3d8ce;
+  // 添加圆角效果
+  top: -20px;
   border-top-left-radius: 20px;
   border-top-right-radius: 20px;
-  background-color: rgb(35, 34, 38);
-  hgroup {
-    display: flex;
-    padding: 10px 20px;
-    text-align: center;
-    h6 {
-      flex: 1;
-      position: relative;
-    }
-    h6:nth-child(2) {
-      flex: 2;
-    }
-    .active::before {
-      position: absolute;
-      content: "";
-      width: 60px;
-      height: 2px;
-      bottom: -9px;
-      border-radius: 5px;
-      transform: translateX(-10%);
-      background-color: #fb7299;
-    }
-  }
+}
+.tab_wrapper {
+  position: fixed;
+  top: 50px;
+  bottom: 50px;
+  height: auto;
+  // overflow: hidden;
+  // overflow-y: scroll;
   li {
     height: 50px;
     font-size: 15px;
