caa19fea1cb76d6fec6e41c4c00abf60d68b3b89
Shi Hai Gang <haigang.shi@foxmail.com>
2019-12-12 21:54:25 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <!-- HTML -->
 <template>
   <div id="app">
-    <!-- header -->
-    <!-- <Header :title="title" :iconLift="iconLift" :iconRight="iconRight" /> -->
-    <!-- content -->
-    <!-- <section class="center"> -->
     <transition name="fade">
       <keep-alive>
         <router-view />
       </keep-alive>
     </transition>
-    <!-- </section> -->
     <!-- footer -->
     <Footer />
   </div>
@@ -18,48 +13,12 @@
 
 <!-- JS -->
 <script type="text/javascript">
-import Header from "components/header.vue";
 import Footer from "components/footer.vue";
 
 export default {
-  data() {
-    return {
-      title: "会员购",
-      iconLift: false,
-      iconRight: false
-    };
-  },
   props: [],
-  watch: {
-    // $route(to, from) {
-    //   switch (true) {
-    //     // 分类
-    //     case to.path.includes("category"):
-    //       this.title = "";
-    //       this.iconLift = true;
-    //       break;
-    //     // 购物车
-    //     case to.path.includes("cart"):
-    //       this.title = "购物车";
-    //       this.iconLift = true;
-    //       break;
-    //     // 订单
-    //     case to.path.includes("order"):
-    //       this.title = "订单";
-    //       this.iconLift = true;
-    //       break;
-    //     // 会员购
-    //     default:
-    //       this.title = "会员购";
-    //       this.iconLift = false;
-    //       break;
-    //   }
-    // }
-  },
-  components: {
-    Header,
-    Footer
-  }
+  watch: {},
+  components: { Footer }
 };
 </script>
 
